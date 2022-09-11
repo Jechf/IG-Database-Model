4e3c4eaa809278de815d0f9fe04d83cf1070b4d8
Jechf <jesuse.chf@gmail.com>
2022-09-11 01:14:26 +0000

    incluir 'password' en tabla user

--- src/models.py
+++ src/models.py
@@ -47,6 +47,7 @@ class User(Base):
     firtsname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
     post = relationship(Post)
     comment = relationship(Comment)
     media = relationship(Media)
